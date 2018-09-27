93f3100336b600c7eb19d027fe80492c6b0fb026
Rodrigo Linares <rlinares@enchufate.pe>
2018-09-27 18:47:41 -0500

    Trying something new.

--- BloodCells/Controllers/Api/ImageController.cs
+++ BloodCells/Controllers/Api/ImageController.cs
@@ -61,7 +61,7 @@ namespace BloodCells.Controllers.Api
 
                 return Ok(ir);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 return InternalServerError();
             }
@@ -69,9 +69,15 @@ namespace BloodCells.Controllers.Api
 
         private Image<Gray, byte> FilterBigCells(Image<Hsv, byte> image)
         {
-            var lowerBlue = new Hsv(90, 80, 160);
-            var upperBlue = new Hsv(150, 140, 255);
-            return image.InRange(lowerBlue, upperBlue);
+            var lower = new Hsv(90, 80, 160);
+            var higher = new Hsv(150, 140, 255);
+
+            //var lower = new Image<Bgr, byte>(HttpContext.Current.Server.MapPath("~/App_Data/low.jpg"))
+            //    .Convert<Hsv, byte>()[0, 0];
+            //var higher = new Image<Bgr, byte>(HttpContext.Current.Server.MapPath("~/App_Data/high.jpg"))
+            //    .Convert<Hsv, byte>()[0, 0];
+
+            return image.InRange(lower, higher);
         }
     }
 }
